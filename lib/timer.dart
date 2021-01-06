d8131ec9d36f16d011e657d823e74026a621b04a
kazukichi <kazukichiyade@Pro.local>
2021-01-06 23:20:25 +0900

    navigator追加

--- lib/timer.dart
+++ lib/timer.dart
@@ -2,36 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'counter.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:riverpod_sample/timer.dart';
-
-void main() {
-  runApp(
-    const ProviderScope(child: MyApp()),
-  );
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MyHomePage(),
-    );
-  }
-}
 
 /// ProviderのCounter選択
 final provider = StateNotifierProvider(
         (ref) => CounterNotifier()
 );
 
-class MyHomePage extends HookWidget {
+class Timer extends HookWidget {
   @override
   Widget build(BuildContext context) {
     final counterModel = useProvider(provider.state);
     return Scaffold(
       appBar: AppBar(
-        title: Text('Counter'),
+        title: Text('Timer'),
       ),
       body: Center(
         child: Column(
@@ -42,12 +25,9 @@ class MyHomePage extends HookWidget {
             Container(
               width: double.infinity,
               child: FlatButton(
-               onPressed: () {
-                 Navigator.of(context).push(MaterialPageRoute(builder: (context) => Timer()));
-               },
-                color: Colors.blue,
-                textColor: Colors.white,
-                child: Text('タイマーへ移動'),
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(builder: (context) => Timer()));
+                },
               ),
             ),
           ],
